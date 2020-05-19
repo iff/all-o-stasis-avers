bc8e3f536a69e92e8202239fb9d6e8d4bdcb74c6
iff <iff@yvesineichen.com>
2020-05-19 06:59:30 +0200

    admin::data: fix types and cleanup

--- client/pages/admin/data.tsx
+++ client/pages/admin/data.tsx
@@ -1,6 +1,5 @@
 import * as Avers from "avers";
 import * as React from "react";
-import Link from "next/link";
 import styled from "styled-components";
 import { CSVLink } from "react-csv";
 
@@ -8,38 +7,49 @@ import { App } from "../../src/app";
 
 import { Site } from "../../src/Views/Components/Site";
 
-import { boulderStats, BoulderStat, gradeCompare } from "../../src/storage";
+import { boulderStats, BoulderStat } from "../../src/storage";
 
-function get_stats(app: App): Any {
-  // very crude way to get the data.. we should at least notify the user when things are ready :)
+interface Event {
+  date: Date;
+  setters: string[];
+  sector: string;
+  grade: string;
+}
+
+function get_stats(app: App): any {
+  // very crude way to get the data.. we should at least notify the user when 
+  // things are ready :)
 
   let accountNames = new Map<string, string>();
   app.data.accountsCollection.ids.get([] as string[]).forEach(accountId => {
     const name = Avers.lookupEditable<Account>(app.data.aversH, accountId)
       .fmap(accountE => {
-        return accountE.content.name == "" ? accountId : accountE.content.name
-      }).get(null);
+        return accountE.content.name
+      }).get("");
 
-    accountNames.set(accountId, name);
+    if (name === undefined || name == "") {
+      accountNames.set(accountId, accountId);
+    } else {
+      accountNames.set(accountId, name);
+    }
   });
 
   const toEvents = (bss: BoulderStat[]) =>
     bss
       .map(
         (bs: BoulderStat): Event[] => {
-          return [{ bs, date: bs.setOn, setters: bs.setters, sector: bs.sector, grade: bs.grade }];
+          return [{ date: bs.setOn, setters: bs.setters, sector: bs.sector, grade: bs.grade }];
         }
       )
       .reduce<Event[]>((a, x) => a.concat(x), [])
       .sort((a, b) => +a.date - +b.date);
 
   let data = Avers.staticValue(app.data.aversH, boulderStats(app.data.aversH)).fmap(toEvents).get<Event[]>([]);
-  let yearly = new Map<string, string[]>();
+  let yearly = new Map<number, string[]>();
   data.forEach( d => {
     const year = d.date.getFullYear();
     let rows = yearly.get(year) || [["setters", "date", "sector", "grade"]];
     let setters = d.setters.map( s => accountNames.get(s) );
-    //console.log(setters);
     rows.push([setters.join(','), d.date, d.sector, d.grade]);
     yearly.set(year, rows);
   });
